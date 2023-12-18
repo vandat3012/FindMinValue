04180bdc55f5fb21b3f93a486a31a0ffaefc1c51
vandat3012 <luongvandat301220@gmail.com>
2023-12-18 15:13:55 +0700

    update bai tap

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@
 public class Main {
     public static void main(String[] args) {
         int arr[] = {1, 5, 7, 2, 4, 5, 6};
-        int min = FindMinElement(arr);
-        System.out.println("the min value in the arr is : " + arr[min]);
+        int newArr = FindMinElement(arr);
+        System.out.println("the min value in the arr is : " + newArr);
     }
 
     public static int FindMinElement(int[] array) {
-        int min = 0;
+        int min = array[0];
         for (int i = 0; i < array.length; i++) {
-            if (array[i] < array[min]) {
-                min = i;
+            if (array[i] < min) {
+                min = array[i];
             }
         }
         return min;
